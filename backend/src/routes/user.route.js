ecacfea41e25f07f09e71bcdd9c21d19e0d08370
Tejash-TS <tejassananse92000@gmail.com>
2025-07-27 19:41:54 +0530

    user routes added

---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,14 @@
+import express from"express"
+import { getUserProfile, syncUser } from "../controllers/user.cotroller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+const router =express.Router();
+//public route
+router.get("/profile/:username",getUserProfile);
+
+//potected routes
+router.post("/sync",protectRoute,syncUser);
+router.post("/me",protectRoute,getCurrentUser);
+router.put("/profile",protectRoute,updateProfile);
+router.post("/follow/:targetUserId",protectRoute,followUser);
+
+export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,17 @@
 import express from "express"
+import cors from "cors"
+import{clerkMiddleware} from "@clerk/express"
+import userRoutes from "./routes/user.route.js"
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 const app = express();
 
+app.use(cors())
+app.use(express.json())
 
+app.use(clerkMiddleware());
 app.get("/",(req,res)=> res.send("hello from server"))
-
+ app.use("/api/user",userRoutes)
 
 // app.listen(ENV.PORT, () =>console.log("Server is up and running on port:",ENV.PORT))
 
