fd0b4f5e9fc24aeddfac86c47f02a638117189cc
Tejash-TS <tejassananse92000@gmail.com>
2025-07-29 08:27:04 +0530

    backend final

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,14 +1,12 @@
 import mongoose from "mongoose";
-import{ ENV } from "./env.js"
+import { ENV } from "./env.js";
 
-
-export const connectDB= async ()=>{
-    try {
-        await mongoose.connect(ENV.MONGO_URI)
-        console.log("DB connected")
-}catch (error){
-console.log("Error connecting to Mongo")
-process.exit(1)
-}
-
-}
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(ENV.MONGO_URI);
+    console.log("Connected to DB SUCCESSFULLY ✅");
+  } catch (error) {
+    console.log("Error connecting to MONGODB");
+    process.exit(1);
+  }
+};
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,14 +1,22 @@
-import express from"express"
-import { getUserProfile, syncUser } from "../controllers/user.cotroller.js";
+import express from "express";
+import {
+  followUser,
+  getCurrentUser,
+  getUserProfile,
+  syncUser,
+  updateProfile,
+} from "../controllers/user.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
-const router =express.Router();
-//public route
-router.get("/profile/:username",getUserProfile);
 
-//potected routes
-router.post("/sync",protectRoute,syncUser);
-router.post("/me",protectRoute,getCurrentUser);
-router.put("/profile",protectRoute,updateProfile);
-router.post("/follow/:targetUserId",protectRoute,followUser);
+const router = express.Router();
+
+// public route
+router.get("/profile/:username", getUserProfile);
+
+// protected routes
+router.post("/sync", protectRoute, syncUser);
+router.get("/me", protectRoute, getCurrentUser);
+router.put("/profile", protectRoute, updateProfile);
+router.post("/follow/:targetUserId", protectRoute, followUser);
 
 export default router;
